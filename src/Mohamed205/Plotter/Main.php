fe24da3b7960db5231c3a43a500779c23e794419
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-09-20 15:57:04 +0200

    EventListener.php toegevoegd WIP

--- src/Mohamed205/Plotter/Main.php
+++ src/Mohamed205/Plotter/Main.php
@@ -7,6 +7,8 @@ namespace Mohamed205\Plotter;
 use CortexPE\Commando\PacketHooker;
 use Mohamed205\Plotter\command\PlotCommand;
 use Mohamed205\Plotter\database\DatabaseManager;
+use Mohamed205\Plotter\listener\EventListener;
+use Mohamed205\Plotter\plot\Plot;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
@@ -23,10 +25,10 @@ class Main extends PluginBase{
         }
 
         $this->getServer()->getCommandMap()->register("plotter", new PlotCommand($this, "plot", "De Plot basecommand"));
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 
         (new DatabaseManager($this))->initDatabase();
-
-
+        
     }
 
     public static function getInstance() : Main
