1fc28f4cf4f197d323fdcb7dfe683154a4d8b6bd
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-09-20 22:44:05 +0200

    ff basic code toegevoegd, nog niet af!

--- src/Mohamed205/Plotter/plot/BuyPlot.php
+++ src/Mohamed205/Plotter/plot/BuyPlot.php
@@ -4,7 +4,23 @@
 namespace Mohamed205\Plotter\plot;
 
 
+use Mohamed205\Plotter\member\Member;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+
 class BuyPlot extends Plot
 {
+
+    private ?int $price;
+    private bool $isSold;
+
+    public function __construct(string $name, ?Member $owner, array $members, Vector3 $minVector, Vector3 $maxVector, Level $level, ?int $price, bool $isSold)
+    {
+        $this->price = $price;
+        $this->isSold = $isSold;
+
+        parent::__construct($name, $owner, $members, $minVector, $maxVector, $level);
+
+    }
 
 }
