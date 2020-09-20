ccc75b14381738a0a3d1b2d877d47386feb56d25
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-09-20 22:47:38 +0200

    testing

--- src/Mohamed205/Plotter/command/PlotAddMemberCommand.php
+++ src/Mohamed205/Plotter/command/PlotAddMemberCommand.php
@@ -6,6 +6,7 @@ namespace Mohamed205\Plotter\command;
 
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
+use Mohamed205\Plotter\plot\Plot;
 use pocketmine\command\CommandSender;
 
 class PlotAddMemberCommand extends BaseSubCommand
@@ -16,14 +17,11 @@ class PlotAddMemberCommand extends BaseSubCommand
      */
     protected function prepare(): void
     {
-        $this->registerArgument(0, new RawStringArgument("member"));
+        //$this->registerArgument(0, new RawStringArgument("member"));
     }
 
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
     {
-        if(isset($args["member"]))
-        {
-            $sender->sendMessage("het lid is toegevoegd");
-        }
+        var_dump(Plot::getAtVector($sender->asVector3(), $sender->getLevel()));
     }
 }
