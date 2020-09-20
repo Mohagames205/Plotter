ad1ab9431317c7db405bb6e0567295759873403d
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-09-20 22:45:43 +0200

    Config saven en itemhelper instance initializeren

--- src/Mohamed205/Plotter/Main.php
+++ src/Mohamed205/Plotter/Main.php
@@ -9,20 +9,25 @@ use Mohamed205\Plotter\command\PlotCommand;
 use Mohamed205\Plotter\database\DatabaseManager;
 use Mohamed205\Plotter\listener\EventListener;
 use Mohamed205\Plotter\plot\Plot;
+use Mohamed205\Plotter\util\ItemHelper;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 
     private static Main $instance;
+    private static ItemHelper $itemHelper;
 
     public function onEnable()
     {
         self::$instance = $this;
+        self::$itemHelper = new ItemHelper($this);
 
         if(!PacketHooker::isRegistered())
         {
             PacketHooker::register($this);
         }
+
+        $this->saveDefaultConfig();
 
         $this->getServer()->getCommandMap()->register("plotter", new PlotCommand($this, "plot", "De Plot basecommand"));
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
@@ -34,6 +39,11 @@ class Main extends PluginBase{
     public static function getInstance() : Main
     {
         return self::$instance;
+    }
+
+    public static function getItemHelper() : ItemHelper
+    {
+        return self::$itemHelper;
     }
 
 
