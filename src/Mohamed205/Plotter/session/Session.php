29ca0671bbb8191af7a0b02e4219f93602b76747
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2020-09-20 15:58:46 +0200

    Plot CreateSession aangemaakt, dit maakt het makkelijker om sessies te beheren

---
+++ src/Mohamed205/Plotter/session/Session.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Mohamed205\Plotter\session;
+
+
+use Mohamed205\Plotter\util\Math;
+use pocketmine\Player;
+
+abstract class Session
+{
+    use Math;
+
+    protected static array $sessions;
+
+    protected static function registerSession(Player $player, Session $session)
+    {
+        self::$sessions[$player->getLowerCaseName()] ??= $session;
+    }
+
+    public static function getSession(Player $player) : Session
+    {
+        return self::$sessions[$player->getLowerCaseName()] ?? new PlotCreateSession($player);
+    }
+
+}
