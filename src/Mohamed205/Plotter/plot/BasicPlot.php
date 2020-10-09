c95fd6bf9562747e030721bc6a067cd07e6d7a30
Mohamed <mohamedelyousfi87@gmail.com>
2020-10-09 20:35:43 +0200

    Een basicplot moet kunnen geconvert worden naar een BuyPlot

--- src/Mohamed205/Plotter/plot/BasicPlot.php
+++ src/Mohamed205/Plotter/plot/BasicPlot.php
@@ -4,12 +4,28 @@
 namespace Mohamed205\Plotter\plot;
 
 
+use Mohamed205\Plotter\database\DatabaseManager;
 use Mohamed205\Plotter\member\Member;
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
 
 class BasicPlot extends Plot
 {
+    public function convertToBuyPlot(?int $price, bool $isBuyable, ?int $playerSellPrice)
+    {
+        $id = $this->getId();
+        $type = BuyPlot::class;
+        $conn = DatabaseManager::getConnection();
+        $stmt = $conn->prepare("UPDATE plots SET plot_type = :type, plot_price = :price, plot_sell_price = :sell_price, plot_is_buyable = :buyable WHERE id = :id");
+        $stmt->bindParam("type", $type);
+        $stmt->bindParam("id", $id);
+        $stmt->bindParam("price", $price);
+        $stmt->bindParam("sell_price", $playerSellPrice);
+        $stmt->bindParam("buyable", $isBuyable);
+        $stmt->execute();
+        $stmt->close();
 
+        return new BuyPlot($this->getName(), $this->getOwner(), $this->getMembers(), $this->getMinVector(), $this->getMaxVector(), $this->getLevel(), $this->getCategory(), $this->getMaxMembers(), $price, $isBuyable, $playerSellPrice);
+    }
 
 }
